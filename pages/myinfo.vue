2a393dfdfc877f7e771b15ae17bc8613ba8cb623
jgunha <42404173+jgunha@users.noreply.github.com>
2024-06-21 14:23:23 +0900

    rollback

--- pages/myinfo.vue
+++ pages/myinfo.vue
@@ -26,42 +26,5 @@ const userStore = useUserStore();
 				},
 			]"
 		/>
-		<div class="container mx-auto p-4 mt-8">
-			<h1 class="text-4xl font-bold mb-8">My Info</h1>
-			<div
-				v-for="member in userStore.members"
-				:key="member.id"
-				class="bg-white shadow-md rounded-lg p-4 mb-4 flex items-center"
-			>
-				<img :src="member.iconUrl || '/default-icon.png'" alt="User Icon" class="w-16 h-16 rounded-full mr-4" />
-				<div>
-				<h2 class="text-2xl font-semibold">{{ member.nickname }}</h2>
-				<p class="text-gray-600">Gender: {{ member.gender }}</p>
-				<p class="text-gray-600">Score: {{ member.activityScore }}</p>
-				<p class="text-gray-600">Tennis Days: {{ tennisDays(member.startDate) }}</p>
-				</div>
-			</div>
-		</div>
 	</PersonalLayout>
 </template>
-
-<script setup>
-import PersonalLayout from '~/layouts/PersonalLayout.vue';
-import { useUserStore } from '~/stores/user.js';
-
-const userStore = useUserStore();
-
-const tennisDays = (startDate) => {
-  const start = new Date(startDate);
-  const today = new Date();
-  const diffTime = Math.abs(today - start);
-  return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-};
-</script>
-
-<style scoped>
-.container {
-  max-width: 800px;
-  margin: 0 auto;
-}
-</style>
